35cc6e9116e78af79b31992aeb9137eaa54c1279
Martins Ikpe <martinsikpe25@gmail.com>
2022-04-28 18:22:01 +0100

    implemented google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "../styles/globals.css"
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+      TagManager.initialize({ gtmId: 'GTM-KL5C8MB' });
+  }, []);
   return <Component {...pageProps} />
 }
 
