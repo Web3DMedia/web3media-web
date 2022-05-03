aa859fac2e479a8827cf1a2d8925f79540a0d0e9
Martins Ikpe <martinsikpe25@gmail.com>
2022-05-03 12:45:11 +0100

    updated cookie banner

--- components/CookiesBanner.tsx
+++ components/CookiesBanner.tsx
@@ -5,23 +5,25 @@ import styled from 'styled-components'
 const CookieBannerContainer = styled.div`
 background: var(--W);
 border-radius: 4px;
-width: 654px;
+width: 600px;
 padding: 25px 40px;
 position: fixed;
-bottom: 20px;
+bottom: 50px;
 left:20px;
 z-index:6;
-@media (max-width: 750px) {
+@media (max-width: 600px) {
 border-radius: 0px;
-width: 348px;
-padding: 25px 24px;
+width: 320px;
+padding: 45px 24px;
 }`
 const Button = styled.button`
-margin-bottom:35px;
+color: var(--Main);
+margin-bottom:50px;
 border-bottom:2px solid var(--Main);
 @media (max-width: 750px) {
 width:100%;
 background: var(--Main);
+color: var(--W)
 margin-top: 34px;
 margin-bottom:0px;
 border-bottom: none;
@@ -40,19 +42,19 @@ const CookieBanner = ({ closecookies }) => {
     }
     return (
         <CookieBannerContainer className='font-body'>
-            <div className='flex justify-end md:justify-start pb-8 ' onClick={declineCookies}>
+            <div className='flex justify-end sm:justify-start pb-8 ' onClick={declineCookies}>
                 <Image src='/images/close-icon.png'  width={10} height={10} alt="Polygon Studios" objectFit='contain' className='cursor-pointer'></Image>
             </div>
-            <div className="text-w md:flex justify-between align-middle">
-                <h1 className='text-black text-lg md:w-4/6'>This website uses cookies to ensure you get the best experience on our website.
+            <div className="text-w sm:flex justify-between align-middle">
+                <h1 className='text-black text-base sm:w-4/6'>This website uses cookies to ensure you get the best experience on our website.
                 <Link href='/cookies-policies'>
                     <a>
-                        <h1 className='text-main underline font-semibold pt-4 md:pt-0'>Learn more</h1>
+                        <h1 className='text-main underline font-semibold pt-4 pb-6 md:pt-0'>Learn more</h1>
                     </a>
                 </Link>
 
                 </h1>
-                <Button className='text-w md:text-base md:text-main font-semibold text-lg' onClick={acceptCookies}>Accept</Button>
+                <Button className='md:text-base font-semibold text-lg' onClick={acceptCookies}>Accept</Button>
             </div>
         </CookieBannerContainer>
     )
